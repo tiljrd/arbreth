5e13fd4e0838af186b4a5bca646d23bd5d5d48d8
til <til@bloctopus.io>
2026-07-07 20:43:59 +0100

    fix(arb-primitives): adapt to reth-core 0.4 trait split

--- crates/arb-primitives/src/receipt.rs
+++ crates/arb-primitives/src/receipt.rs
@@ -729,7 +729,7 @@ impl reth_db_api::table::Compress for ArbReceipt {
 }
 
 impl reth_db_api::table::Decompress for ArbReceipt {
-    fn decompress(value: &[u8]) -> Result<Self, reth_db_api::DatabaseError> {
+    fn decompress(value: &[u8]) -> Result<Self, reth_codecs::DecompressError> {
         let (obj, _) = reth_codecs::Compact::from_compact(value, value.len());
         Ok(obj)
     }
